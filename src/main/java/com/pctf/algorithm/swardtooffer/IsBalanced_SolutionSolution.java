94c771d81be0576dc77b407313a4353edd7c5dce
Switcherman <418211500@163.com>
2018-08-01 13:58:52 +0800

    平衡二叉树单次遍历解法

--- src/main/java/com/pctf/algorithm/swardtooffer/IsBalanced_SolutionSolution.java
+++ src/main/java/com/pctf/algorithm/swardtooffer/IsBalanced_SolutionSolution.java
@@ -2,13 +2,31 @@ package com.pctf.algorithm.swardtooffer;
 
 /**
  * 判断一个二叉树是否是平衡二叉树
+ * 思路1:遍历左右子树,深度差绝对值<=1 ,递归执行
+ * 思路2:遍历左右子树,深度差大于1 直接返回.
  */
 public class IsBalanced_SolutionSolution {
     public boolean IsBalanced_Solution(TreeNode root) {
         if(root == null) {
             return true;
         }
-        return Math.abs(height(root.right) - height(root.left)) <= 1 && IsBalanced_Solution(root.left) && IsBalanced_Solution(root.right);
+        return getDepth(root) != -1;
+        // return Math.abs(height(root.right) - height(root.left)) <= 1 && IsBalanced_Solution(root.left) && IsBalanced_Solution(root.right);
+    }
+
+    private int getDepth(TreeNode node) {
+        if(node == null) {
+            return 0;
+        }
+        int left = getDepth(node.left);
+        if(left == -1) {
+            return -1;
+        }
+        int right = getDepth(node.right);
+        if(right == -1) {
+            return -1;
+        }
+        return Math.abs(left - right) > 1 ? -1 : 1 + Math.max(left, right);
     }
 
     private int height(TreeNode node) {
