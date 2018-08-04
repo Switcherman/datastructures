3be44264ed002298eb58324c635bccdb3a439fa7
Switcherman <418211500@163.com>
2018-08-04 23:37:16 +0800

    数组中只出现一次的数字

--- src/main/java/com/pctf/algorithm/swardtooffer/FindNumsAppearOnceSolution.java
+++ src/main/java/com/pctf/algorithm/swardtooffer/FindNumsAppearOnceSolution.java
@@ -3,6 +3,10 @@ package com.pctf.algorithm.swardtooffer;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * 题目描述
+ * 一个整型数组里除了两个数字之外，其他的数字都出现了偶数次。请写程序找出这两个只出现一次的数字。
+ */
 public class FindNumsAppearOnceSolution {
 
     public void FindNumsAppearOnce(int [] array,int num1[] , int num2[]) {
